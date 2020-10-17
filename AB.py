eb2dab74cf7756cf41bd60f1021d8d0608c2d860
Miller162 <millertimlin@cmail.carleton.ca>
2020-10-17 14:21:23 -0400

    [Miller Timlin] docs: step 5

--- AB.py
+++ AB.py
@@ -1,6 +1,8 @@
 # SYSC3010 Lab 5 - AB.py
 #     The file to be jointly edited by Team Members A and B
 
+from A import * #this line was added by Team member A
+
 def function_AB():
     print ("AB")
- 
+    function_A() #this line was added by Team member A
