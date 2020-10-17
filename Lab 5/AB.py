554c825ac9d0c3a11632a1069db3659d87b4a221
gillgurjit <71581824+gillgurjit@users.noreply.github.com>
2020-10-17 14:45:18 -0400

    Update AB.py

--- Lab 5/AB.py
+++ Lab 5/AB.py
@@ -1,6 +1,9 @@
 # SYSC3010 Lab 5 - AB.py
 #     The file to be jointly edited by Team Members A and B
 
+from B import *    #Line is added by Team Member B
+
 def function_AB():
+    function_B()  #Line is added by Team Member B
     print ("AB")
  
