b5b9cf748cfb2a7ccc06c6998c6f411cf9559e6d
Miller162 <millertimlin@cmail.carleton.ca>
2020-11-28 15:17:42 -0500

    [Miller Timlin] Documentation/Debugging: Near-Final iteration for RPI1 code
    This code culminates much of the code from the testing phase.  this could be considered the final protoptyping phase

--- RPI1/RPI1_Motor_test.py
+++ RPI1/RPI1_Motor_test.py
@@ -11,55 +11,52 @@ This file can be imported as a module with the following functions:
     *down - causes the motore to roll down
 """
 
-#TESTING import statements
-try: 
+#TESTING import statements------------------------------------------------------
+try:
     import RPi.GPIO as GPIO
-    
 except ImportError:
-    print ("GPIO module could not be imported")
-    
+    print ("GPIO module could not be imported (Motor)")
 else:
-    print ("GPIO module imported successfully")
-    
+    print ("GPIO module imported successfully (Motor)")
 #END try-except
-    
+
 try:
     from time import sleep
-    
 except ImportError:
-    print ("Time module could not be imported")
-    
+    print ("Time module could not be imported (Motor)")
 else:
-    print ("Time module imported successfully")
+    print ("Time module imported successfully (Motor)")
 #END try-except
-    
-def down():
+
+
+#TESTING functions--------------------------------------------------------------
+def down(time):
     """A function that causes the motors to roll down"""
-    print("down")
+    print("Blinds down")
     GPIO.output(17, GPIO.HIGH) #set input 1 to logic high
-    GPIO.output(27, GPIO.LOW) #set input 2 to logic low    
-    
-    sleep(10)
-    
+    GPIO.output(27, GPIO.LOW) #set input 2 to logic low
+
+    sleep(time)
+
     #reset values to logic low
-    GPIO.output(17, GPIO.LOW) 
+    GPIO.output(17, GPIO.LOW)
     GPIO.output(27, GPIO.LOW)
 #END clockwise
 
-def up():
+def up(time):
     """A function that causes the motors to roll up"""
-    print("up")
+    print("Blinds up")
     GPIO.output(17, GPIO.LOW) #set input 1 to logic low
     GPIO.output(27, GPIO.HIGH) #set input 2 to logic high
-    
-    sleep(10)
-    
+
+    sleep(time)
+
     #reset values to logic low
     GPIO.output(17, GPIO.LOW)
     GPIO.output(27, GPIO.LOW)
 #END counter_clockwise
 
-def Motor_test():
+def motor_test():
     """A function that runs various tests on the initiialization and implementation of the external motors
 
     This function tests the GPIO pin setup, and runs a script that should cause the motors to roll all the way up and then unroll completely.
@@ -74,22 +71,21 @@ def Motor_test():
         GPIO.setup(4, GPIO.OUT) #Motor enable
         GPIO.setup(17, GPIO.OUT) #H-Bridge input 1
         GPIO.setup(27, GPIO.OUT) #H-Bridge input 2
-        
     except:
-        print ("GPIO setup error")
-        
+        print ("Motor GPIO setup error")
     else:
-        print ("GPIO setup successfully")
+        print ("Motor GPIO setup successfully")
     #END try-except
-    
+
     #Enable H-bridge output
     GPIO.output(4, GPIO.HIGH)
-    
-    up()
+
+    up(10.5)
     sleep(3)
-    down()
-    
+    down(10.5)
+
     GPIO.cleanup() #resetting pins to defaults
-#END Motor_test
-    
-#Motor_test()  #for use when run as an individual file. commented out when in use as an importable library in main test file
+    print("Motor test program run successfully \n")
+#END motor_test
+
+#motor_test()  #for use when run as an individual file. commented out when in use as an importable library in main test file
--- RPI1/RPI1_PiCam_test.py
+++ RPI1/RPI1_PiCam_test.py
@@ -13,25 +13,25 @@ This file can be imported as a module with the following functions:
 try:
     import picamera
     #from picamera import PiCamera
-    
+
 except ImportError:
     print("PiCamera module could not be imported")
-    
+
 else:
     print("PiCamera module imported succesfully")
 #END try-except
-    
-try: 
+
+try:
     from PIL import Image
-    
+
 except ImportError:
     print("PIL module could not be imported")
-    
+
 else:
     print("PIL module imported successfully")
-    
+
 #END try-except
-    
+
 #TESTING PiCamera
 def PiCam_test():
     """A function that runs various tests on the initiialization and implementation of the external Raspberry Pi Camera
@@ -40,7 +40,7 @@ def PiCam_test():
     It then tests the image processing library by causing it to load that image and calculate the average relative lumenance of the image.
     Both of these tests can produce multiple different kinds of errors, most of which will be captured and properly identified by this test case.
     """
-    
+    #TESTING PiCam
     try:
         WIDTH = 64 #width for picture capture
         HEIGHT = 64 #height for picture capture
@@ -49,48 +49,42 @@ def PiCam_test():
         camera.resolution = (WIDTH, HEIGHT) #set resolution (keep as small as possible to avoid lengthy calculations in the future)
         camera.capture('/home/pi/Desktop/image.jpg') #camera take picture and save to directory
         camera.close()#close the camera
-        
-    except FileNotFoundError: 
+    except FileNotFoundError:
         print ("Pi Camera could not save image")
-        
     except picamera.exc.PiCameraRuntimeError:
         print ("Invalid Pi Camera operation performed during runtime")
-        
     except picamera.exc.PiCameraClosed:
         print ("Method called on closed Pi Camera instance")
-        
     else:
-        print ("PiCamera has run successfully")
+        print ("PiCamera test program has run successfully \n")
     #END try-except
-        
+
     #TESTING PIL
     try:
         img = Image.open("/home/pi/Desktop/image.jpg") #opens image from location saved by the PiCamera
         luma=0 #sum of the luma of each pixels
         pixels = img.width*img.height #number of pixels in the picture
-        
+
         if (pixels != WIDTH*HEIGHT): #if the expected number of pixels match the actual (same as comparing expected height and width of the image
             print("Expected width and height do not match actual")
         #END if
-            
+
         for x in range(img.width): #for pixel in width
             for y in range(img.height): #for pixel in height
-                (r, g, b) = img.getpixel((x,y))#get colour touple 
+                (r, g, b) = img.getpixel((x,y))#get colour touple
                 luma += (0.2126*r + 0.7152*g + 0.0722*b) #calculate luma of RGB data, then add to total
             #END for
         #END for
         img.close()#ensure to properly close the image
-        
     except FileNotFoundError:
         print ("Could not find image to be opened")
-        
     except IndexError:
         print ("Attempting to access pixels out of range")
-        
     else:
-        print("PIL has run successfully")
         print("The average luma is: %d" % (luma/pixels))
+        print("PIL test program has run successfully \n")
+        
     #END try-except
-#END PiCam_test   
+#END PiCam_test
 
 #PiCam_test() #for use when run as an individual file. commented out when in use as an importable library in main test file
