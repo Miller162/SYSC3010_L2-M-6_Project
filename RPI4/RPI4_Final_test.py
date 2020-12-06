46c7d0a60b34735106b9a105702d60b1ddb4999f
Eric Hardy <eric.hardy@carleton.ca>
2020-12-06 11:43:30 -0500

    [Eric Hardy] Implementation: Final implementation

--- RPI4/RPI4_Final_test.py
+++
@@ -1,6 +0,0 @@
-import RPI4_Final
-from time import sleep
-
-while(1):
-    RPI4_Final.main(85, False, 1, 1)
-    sleep(4)
---
+++ RPI4/emailAlert.py
@@ -0,0 +1,33 @@
+import smtplib
+
+#Login Credentials
+emailAddress = 'hemas.assistant@gmail.com'
+emailPassword = 'TheEarthIsFlat'
+server = 'smtp.gmail.com'
+port = 587
+
+class EmailNotification:
+    def notifyUser(self, recipient, subject, message):
+        
+        headers = ["From: " + emailAddress, "Subject: " + subject, "To: " + recipient, "MIME-Version: 1.0", "Content-Type: text/html"]
+        headers = "\r\n".join(headers)
+        
+        # Establish connection to Gmail server
+        session = smtplib.SMTP(server, port)
+        session.ehlo()
+        session.starttls()
+        session.ehlo()
+        session.login(emailAddress, emailPassword)
+        
+        # Combine email contents and send
+        session.sendmail(emailAddress, recipient, headers + "\r\n\r\n" + message)
+        
+        session.quit   # end the session
+
+# The following code tests the email notifcation system
+# This is one example of its implementation
+email = EmailNotification()
+recipient = 'hemas.assistant@gmail.com'
+subject = "WARNING: High Temperature"
+message = "The current temperature in the house exceeds the set threshold. User action is required."
+email.notifyUser(recipient, subject, message)
